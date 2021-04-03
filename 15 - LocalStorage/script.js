e0390880fb18de53aa5f85f719c4074fbb4c92fb
Tom Chestnut <tchestnut85@gmail.com>
2021-04-03 17:40:08 -0400

    Completed day 15 - LocalStorage and event delegation.

---
+++ 15 - LocalStorage/script.js
@@ -0,0 +1,53 @@
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+const deleteBtn = document.querySelector('#delete-btn');
+const items = JSON.parse(localStorage.getItem('items')) || [];
+
+// TODO-1: Add function to check and uncheck all list items
+
+function addItem(e) {
+	e.preventDefault();
+	const text = this.querySelector('[name=item]').value;
+	const item = {
+		text,
+		done: false,
+	};
+
+	items.push(item);
+	populateList(items, itemsList);
+	localStorage.setItem('items', JSON.stringify(items));
+	this.reset();
+}
+
+function populateList(plates = [], platesList) {
+	platesList.innerHTML = plates
+		.map((plate, i) => {
+			return `
+            <li>
+                <input type='checkbox' data-index=${i} id='item${i}' ${plate.done ? 'checked' : ''} />
+                <label for='item${i}'>${plate.text}</label>
+            </li>
+        `;
+		})
+		.join('');
+}
+
+function toggleDone(e) {
+	if (!e.target.matches('input')) return; // skip this unless its an input
+	const el = e.target;
+	const index = el.dataset.index;
+	items[index].done = !items[index].done;
+	localStorage.setItem('items', JSON.stringify(items));
+	populateList(items, itemsList);
+}
+
+function deleteItems() {
+	localStorage.removeItem('items');
+	location.reload();
+}
+
+addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
+deleteBtn.addEventListener('click', deleteItems);
+
+populateList(items, itemsList);
