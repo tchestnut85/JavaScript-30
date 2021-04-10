d0ecd08863b5d9e2160c4f4eca9eea1364bebbc5
Tom Chestnut <tchestnut85@gmail.com>
2021-04-09 20:09:49 -0400

    Completed day 22 - Follow along link highlight.

---
+++ 22 - Follow Along Link Highlighter/script.js
@@ -0,0 +1,23 @@
+const triggers = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.append(highlight);
+
+function highlightLink() {
+	// console.log(this);
+	const linkCoords = this.getBoundingClientRect();
+	// console.log(linkCoords);
+
+	const coords = {
+		width: linkCoords.width,
+		height: linkCoords.height,
+		top: linkCoords.top + window.scrollY,
+		left: linkCoords.left + window.scrollX,
+	};
+
+	highlight.style.width = `${linkCoords.width}px`;
+	highlight.style.height = `${linkCoords.height}px`;
+	highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`;
+}
+
+triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
